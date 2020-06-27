fce614f09c370a7c975f98be1c7bdeb256761310
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-28 00:13:30 +0100

    Class outline

---
+++ create_playlist.py
@@ -0,0 +1,24 @@
+class CreatePlaylist:
+
+    def __init__(self):
+        pass
+
+    # Step 1: Log into YouTube
+    def get_youtube_client(self):
+        pass
+
+    # Step 2: Grab the liked videos
+    def get_liked_videos(self):
+        pass
+
+    # Step 3: Create a new Playlist
+    def create_playlist(self):
+        pass
+
+    # Step 4: Search for the Song
+    def get_spotify_url(self):
+        pass
+
+    # Step 5: Add this song into the new Spotify playlist
+    def add_song_to_playlist(self):
+        pass
