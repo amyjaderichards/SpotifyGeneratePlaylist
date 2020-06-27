6b1390326349823ffa91cb80c9a51d0e46aa101a
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-28 00:35:35 +0100

    create_playlist function finished

--- create_playlist.py
+++ create_playlist.py
@@ -1,3 +1,14 @@
+#Step 1: Log into YouTube
+#Step 2: Grab the liked videos
+#Step 3: Create a New Playlist
+#Step 4: Search for the Song
+#Step 5: Add this song into the new Spotify playlist
+
+import json
+import requests
+from secrets import spotify_user_id, spotify_token
+
+
 class CreatePlaylist:
 
     def __init__(self):
@@ -13,7 +24,24 @@ class CreatePlaylist:
 
     # Step 3: Create a new Playlist
     def create_playlist(self):
-        pass
+        request_body = json.dumps({
+            "name": "YouTube Liked Vids",
+            "description": "All Liked YouTube Video",
+            "public": True
+        })
+
+        query = "https://api.spotify.com/v1/users/{user_id}/playlists".format(self.user_id)
+        response = requests.post(
+            query,
+            data=request_body,
+            headers={
+                "Content-Type": "application.json",
+                "Authorization": "Bearer {}".format(spotify_token)
+            }
+        )
+        response_json = response.json()
+        # Playlist id
+        return response_json("id")
 
     # Step 4: Search for the Song
     def get_spotify_url(self):
